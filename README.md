ba5a7d880eac471977a44dd80993c9e66b0dba32
Gonçalo Vieira <sandwave@gmail.com>
2016-04-22 21:17:17 +0100

    initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # cookie-wrapper
-A Wrapper for handling cookies in a concise and easy way in TypeScript
+Wraps Cookie sessions into a decent-to-use class for TypeScript projects
+## Allows for most usual operations done over cookies, ie:
+ *  get a Cookie - getKey(<key:string>)
+ *  set a Cookie - setKey(<key:string>, <value:string>)
+ *  remove a Cookie - removeKey(<key:string>)
+
+## Example:
+
+```TypeScript
+    import {CookieWrapper} from "cookie.wrapper";
+    
+    export class HowdyHo {
+        private message: string;
+        
+        constructor(private cookieWrapper: CookieWrapper) {
+            this.cookieWrapper.setKey("Eh", "a cookie!");
+            this.message = this.cookieWrapper.getKey("Eh");
+        }
+    }
+```
+
+## List of TODOs:
+ *  A Cookie interface
+ *  Keys as an Observable
+ *  Possibly wrappers for frameworks
---
+++ src/cookie.wrapper.ts
@@ -0,0 +1,54 @@
+/**
+ * Wraps Cookie sessions into a decent-to-use class for TypeScript projects
+ * Allows for most usual operations done over cookies, ie:
+ *  - get a Cookie - getKey(<key:string>)
+ *  - set a Cookie - setKey(<key:string>, <value:string>)
+ *  - remove a Cookie - removeKey(<key:string>)
+ *
+ *  TODO: A Cookie interface
+ *  TODO: Keys as an Observable
+ */
+export default class CookieWrapper {
+    private keys: any;
+
+    constructor() {
+        this.parseKeys();
+    }
+
+    /**
+     * Populates this.keys as a Javascript Object
+     */
+    private parseKeys() {
+        this.keys = JSON.parse(`{"${document.cookie.replace(/; /g, ",").replace(/=/g, `":"`)}"}`);
+    }
+
+    /**
+     * Returns a key stored in the Cookie session
+     * @param key
+     * @returns {any}
+     */
+    public getKey(key: string) {
+        if (!this.keys[key]) this.parseKeys();
+        if (!this.keys[key]) throw new Error(`Cookie "${key}" does not exist.`);
+        return this.keys[key];
+    }
+
+    /**
+     * Sets a cookie in the Cookie session
+     * @param key
+     * @param value
+     */
+    public setKey(key: string, value: string | number) {
+        this.keys[key] = value;
+        document.cookie = `${key}=${value};`;
+    }
+
+    /**
+     * Removes a cookie from the Cookie session
+     * @param key
+     */
+    public removeKey(key: string) {
+        delete this.keys[key];
+        this.setKey(key, "; expires=Thu, 01 Jan 1970 00:00:01 GMT");
+    }
+}
