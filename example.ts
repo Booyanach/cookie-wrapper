5695d3a7dc008291c61744ef858333ac6602d469
Gonçalo Vieira <sandwave@gmail.com>
2016-04-22 21:42:37 +0100

    adding example file.

---
+++ example.ts
@@ -0,0 +1,10 @@
+import {CookieWrapper} from "cookie.wrapper";
+
+export class HowdyHo {
+    private message: string;
+
+    constructor(private cookieWrapper: CookieWrapper) {
+        this.cookieWrapper.setKey("Eh", "a cookie!");
+        this.message = this.cookieWrapper.getKey("Eh");
+    }
+}
