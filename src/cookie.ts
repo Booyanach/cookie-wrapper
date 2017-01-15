f59403333805a593806c3f057bb410495fc18df8
Gonçalo Vieira <sandwave@gmail.com>
2017-01-15 12:12:17 +0000

    Cookies are now an object of their own. CookieWrapper now works over Cookie objects.

---
+++ src/cookie.ts
@@ -0,0 +1,65 @@
+/**
+ * A class that represents a browser's Cookie string, in an object format
+ * Allows for:
+ *  - creating a Cookie - new Cookie(
+ *      <key:string>, <value:string>, <domain:?string>, <expires:?string>, <path:?string>
+ * )
+ *  - delete a Cookie - delete()
+ *  - convert a Cookie to a browser Cookie string - toCookieString()
+ *  - save a Cookie - save()
+ *
+ *  TODO: endpoint to serve a Cookie as an Observable
+ */
+export default class Cookie {
+    key: string;
+    path: string;
+    value: string;
+    domain: string;
+    expires: string;
+
+    constructor(
+        key: string, value: string, domain: string = '', expires: string = '', path: string = '/'
+    ) {
+        this.key = key;
+        this.path = path;
+        this.value = value;
+        this.domain = domain;
+        this.expires = expires;
+
+        this.save();
+    }
+
+    /**
+     * Deletes this cookie, ie, sets the expiry date to before current date.
+     */
+    public delete() {
+        this.expires = 'Thu, 01 Jan 1970 00:00:01 GMT';
+        this.path = '/';
+        this.save();
+    }
+
+    /**
+     * Generates a string in the format that document.cookie reads.
+     */
+    public toCookieString() {
+        let cookie = `${this.key}=${this.value};`;
+
+        if (this.expires) {
+            cookie += `expires=${this.expires};`;
+        }
+        if (this.domain) {
+            cookie += `domain=${this.domain};`;
+        }
+        if (this.path) {
+            cookie += `path=${this.path};`;
+        }
+        return cookie;
+    }
+
+    /**
+     * Stores this cookie to the current document.
+     */
+    public save() {
+        document.cookie = this.toCookieString();
+    }
+}
--- src/cookie.wrapper.ts
+++ src/cookie.wrapper.ts
@@ -1,16 +1,15 @@
+import Cookie from './cookie';
 /**
- * Wraps Cookie sessions into a decent-to-use class for TypeScript projects
+ * Wraps Cookie sessions into a decent-to-use class for TypeScript projects.
+ * 
  * Allows for most usual operations done over cookies, ie:
  *  - get a Cookie - getKey(<key:string>)
  *  - set a Cookie - setKey(<key:string>, <value:string>)
  *  - remove a Cookie - removeKey(<key:string>)
  *  - list all Cookie keys - queryKeys()
- *
- *  TODO: A Cookie interface
- *  TODO: Keys as an Observable
  */
 export default class CookieWrapper {
-    private keys: any;
+    private keys: any = {};
 
     constructor(private domain: string = "") {
         this.parseKeys();
@@ -19,17 +18,12 @@ export default class CookieWrapper {
     /**
      * Returns a key stored in the Cookie session
      * @param key
-     * @returns {any}
+     * @returns {Cookie}
      */
-    public getKey(key: string) {
+    public getKey(key: string): Cookie {
         this.parseKeys();
         if (!this.keys[key]) return undefined;
-        var decoded = decodeURIComponent(this.keys[key]);
-        try {
-            return JSON.parse(decoded);
-        } catch (e) {
-            return decoded;
-        }
+        return this.keys[key];
     }
 
     /**
@@ -37,36 +31,29 @@ export default class CookieWrapper {
      * @param key
      * @param value
      * @param expiration
+     * @returns {Cookie}
      */
-    public setKey(key: string, value: any, expiration: string = '') {
-        this.keys[key] = encodeURIComponent(JSON.stringify(value));
-
-        let cookie = `${key}=${this.keys[key]};`;
-
-        if (expiration) {
-            cookie += `expires=${expiration};`;
-        }
-        if (this.domain) {
-            cookie += `domain=${this.domain};`;
-        }
-
-        document.cookie = cookie;
+    public setKey(key: string, value: any, expiration: string = ''): Cookie {
+        let cookie: Cookie = new Cookie(key, value, this.domain, expiration);
+        this.keys[key] = cookie;
+        return cookie;
     }
 
     /**
      * Removes a cookie from the Cookie session
+     * @deprecated: use the cookie object itself after using getKey
      * @param key
      */
     public removeKey(key: string) {
-        delete this.keys[key];
-        this.setKey(key, '', 'Thu, 01 Jan 1970 00:00:01 GMT');
+        let cookie = this.keys[key];
+        cookie.delete();
     }
 
     /**
      * Returns a list of all the Cookie keys
-     * @returns {string[]}
+     * @returns {Array<string>}
      */
-    public queryKeys() {
+    public queryKeys(): Array<string> {
         this.parseKeys();
         if (!Object.keys(this.keys)) return [];
         return Object.keys(this.keys);
@@ -76,28 +63,38 @@ export default class CookieWrapper {
      * Sets Expiration date on a key
      * @param key
      * @param days
-     * @returns {string[]}
+     * @returns {Cookie}
      */
-    public setExpiration(key: string, days: number) {
+    public setExpiration(key: string, days: number): Cookie {
         this.parseKeys();
         let expiration = new Date();
         expiration.setDate(expiration.getDate() + days);
-        this.setKey(key, this.getKey(key), expiration.toISOString());
+        let cookie = this.getKey(key);
+        cookie.expires = expiration.toISOString();
+        cookie.save();
+        return cookie;
     }
 
     /**
-     * Populates this.keys as a Javascript Object
+     * Populates this.keys as a Javascript Object indexing all Cookie objects currently in the
+     * browser session
      */
     private parseKeys() {
         let crumbs = document.cookie.split("; ");
         if (!crumbs[crumbs.length-1]) {
             crumbs.pop();
         }
-        this.keys = JSON.parse("{" +
+        let keys = JSON.parse("{" +
             crumbs.map(crumb => {
                 let crumbArr = crumb.split(/=/);
                 return `"${crumbArr[0]}": "${crumbArr[1]}"`;
             }).join(",") +
         "}");
+        this.keys = {};
+        Object.keys(keys).forEach(key => {
+            this.keys[key] = new Cookie(
+                key, keys[key].value, keys[key].domain, keys[key].expires, keys[key].path
+            );
+        });
     }
 }
